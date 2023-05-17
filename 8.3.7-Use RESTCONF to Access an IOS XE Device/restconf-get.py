e63f29f3cc627de73d1ff2a52bb2f3317286ab0a
HuangK2001 <hi.huang@foxmail.com>
2023-05-17 12:01:04 +0000

    Use RESTCONF to Access an IOS XE Device

---
+++ 8.3.7-Use RESTCONF to Access an IOS XE Device/restconf-get.py
@@ -0,0 +1,14 @@
+import json
+import requests
+requests.packages.urllib3.disable_warnings()
+api_url = "https://192.168.56.102/restconf/data/ietf-interfaces:interfaces"
+headers = { "Accept": "application/yang-data+json",
+ "Content-type":"application/yang-data+json"
+}
+basicauth = ("cisco", "cisco123!")
+resp = requests.get(api_url, auth=basicauth, headers=headers, verify=False)
+print(resp)
+
+response_json = resp.json()
+# print(response_json)
+print(json.dumps(response_json, indent=4))
